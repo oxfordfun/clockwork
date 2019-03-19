f9b8eb1acc1a9c98f128e11a685fc20cfb6faf26
Martin Hunt <martin.g.hunt@gmail.com>
2019-03-19 13:38:15 +0000

    Fix permissions of bayestyper files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -210,6 +210,7 @@ rm KMC3.linux.tar.gz
 cd $install_root
 wget https://github.com/bioinformatics-centre/BayesTyper/releases/download/v1.3.1/bayesTyper_v1.3.1_linux_x86_64.tar.gz
 tar xf bayesTyper_v1.3.1_linux_x86_64.tar.gz
+chmod --recursive 755 bayesTyper_v1.3.1_linux_x86_64/
 rm bayesTyper_v1.3.1_linux_x86_64.tar.gz
 cp -s bayesTyper_v1.3.1_linux_x86_64/bin/* .
 
