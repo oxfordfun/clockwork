984f274178c9fb4e1d4c5ae20722bd073b292c2b
Martin Hunt <martin.g.hunt@gmail.com>
2019-03-19 13:36:00 +0000

    Tidy up tarballs

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -203,11 +203,13 @@ pip3 install bio-minos==0.5.1
 cd $install_root
 wget https://github.com/refresh-bio/KMC/releases/download/v3.0.0/KMC3.linux.tar.gz
 tar xf KMC3.linux.tar.gz
+rm KMC3.linux.tar.gz
 
 
 #____________________ bayesTyper ____________________________#
 cd $install_root
 wget https://github.com/bioinformatics-centre/BayesTyper/releases/download/v1.3.1/bayesTyper_v1.3.1_linux_x86_64.tar.gz
 tar xf bayesTyper_v1.3.1_linux_x86_64.tar.gz
+rm bayesTyper_v1.3.1_linux_x86_64.tar.gz
 cp -s bayesTyper_v1.3.1_linux_x86_64/bin/* .
 
